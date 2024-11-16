c8f777312404241557248c40dacfd34b751a211a
Yaswant pandey <122995011+indianhacker12@users.noreply.github.com>
2024-11-16 18:49:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify, send_from_directory
-from flask_sqlalchemy import SQLAlchemy
+
 from werkzeug.security import generate_password_hash, check_password_hash
 import razorpay
 from datetime import datetime
